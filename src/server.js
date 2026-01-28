9296a4d76a688101127c08fac4fe0a319c3c8c84
nattanjunior <fnatanieljunior@gmail.com>
2026-01-28 17:26:25 -0300

    refactor server.js to improve request handling and response structure

--- src/server.js
+++ src/server.js
@@ -1,10 +1,32 @@
 import { createServer } from "node:http"
 
+import { validateCPF } from './utils/cpfvalidator.js';
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  const { url, method } = req;
+
+  if (url === '/' && method === 'GET') {
+    res.end('Hello programmer!');
+  }
+
+  if (url === '/validator' && method === 'POST') {
+   
+
+    req.on('end', async () => {
+      const { cpf } = JSON.parse(req.body);
+      const result = await validateCPF(cpf);
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(result));
+    });
+  }
+
+});
+
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
